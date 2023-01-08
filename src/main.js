8bc5e883d8db8392b191c27a5fdd8420a11934fb
Robson de Moraes Silva <rms2307@outlook.com>
2023-01-08 15:32:04 -0300

    Criando uma diretiva personalizada

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const Vue = createApp(App)
+
+Vue.directive('teste',{
+    created(){
+        console.log("Diretiva Aplicada...")
+    }
+})
+
+Vue.mount('#app')
